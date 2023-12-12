012bb4f12cacc33ea65abb8c81067973ceb3b495
Federico Decono <fdecono@gmail.com>
2023-12-12 13:13:12 -0300

    Add authentication Singin

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from "@nestjs/common";
+import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { randomBytes, scrypt as _scrypt } from "crypto";
 import { promisify } from "util";
@@ -10,30 +10,33 @@ export class AuthService {
   constructor(private usersService: UsersService) {}
 
   async signup(email: string, password: string) {
-    // See if email is in use
     const users = await this.usersService.find(email);
     if (users.length) {
       throw new BadRequestException('email is in use');
     }
 
-    // hash pass
-    // generate a salt
     const salt = randomBytes(8).toString('hex');
-
-    // hash salt + pass
     const hash = (await scrypt(password, salt, 32)) as Buffer;
-
-    // join hashed result + salt
     const result = salt + '.' + hash.toString('hex');
-
-    // create new user and save it
     const user = await this.usersService.create(email, result);
 
-    // return it
     return user;
   }
 
-  signin() {
+  async signin(email: string, password: string) {
+    const [user] = await this.usersService.find(email);
+    if (!user) {
+      throw new NotFoundException('user not found');
+    }
 
+    const [salt, storedHash] = user.password.split('.');
+
+    const hash = (await scrypt(password, salt, 32)) as Buffer;
+
+    if (storedHash !== hash.toString('hex')) {
+      throw new BadRequestException('bad password');
+    }
+
+    return user;
   }
 }
